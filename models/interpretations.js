8e60c6b46006594328f198b23773dc182870ce0a
demichele-c <demichele.charles@yahoo.com>
2024-08-20 20:42:59 -0400

    updated page to use mvc logic

---
+++ models/interpretations.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection'); // Adjust path as needed
+
+class Interpretation extends Model {}
+
+Interpretation.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        dream_description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        interpretation_text: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        created_at: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+    },
+    {
+        sequelize,
+        modelName: 'Interpretation',
+        tableName: 'interpretations',
+        timestamps: false,
+    }
+);
+
+module.exports = Interpretation;
